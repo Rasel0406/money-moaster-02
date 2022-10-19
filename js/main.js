4c34b17b756ae966a6053941a75e0855a33c7c65
Rasel0406 <durjoychowdhury0406@gmail.com>
2022-10-20 01:34:26 +0600

    file is compelete

--- js/main.js
+++ js/main.js
@@ -8,23 +8,42 @@ function inputFiled(input) {
     return expensesValue;
 }
 
-function expenses(expensesFiled) {
+ function expenses(expensesFiled) {
     const total = document.getElementById(expensesFiled)
-    const totalValue = total.innerText
-    return totalValue;
-}
-document.getElementById('calculate-button').addEventListener("click",function(){
-    const incomeAmount = inputFiled("income-filed");
+    return total;
+} 
+
+function totalExpensesAmount() {
     const rentAmount = inputFiled("rent-filed");
     const clothes = inputFiled("clothes-filed");
     const foodAmount = inputFiled("food-filed");
-    const totalExpenses = expenses("total-expenses");
+    const totalExpenses = foodAmount + rentAmount + clothes;
+    return totalExpenses;
+}
+document.getElementById('calculate-button').addEventListener("click",function(){
+    const incomeAmount = inputFiled("income-filed");
+    const expensesTotal = expenses("total-expenses");
     const balance = expenses("balance");
-    const totalExpensesAmount = rentAmount + foodAmount + clothes:
-    totalExpenses.innerText = totalExpensesAmount:
-    const newBalance = incomeAmount - totalExpensesAmount;
-    balance.innerText = newBalance;
-    // console.log(incomeAmount,foodAmount,rentAmount,clothes,totalExpenses,balance)
+    const totalExpenses = totalExpensesAmount();
+    // console.log(incomeAmount , foodAmount , rentAmount, clothes, expensesTotal,balance)
+    const remainingBalance = incomeAmount - totalExpenses;
+    expensesTotal.innerText = totalExpenses;
+    balance.innerText = remainingBalance;
+   
+    //  console.log(totalExpenses, remainingBalance);
+})
 
+// saving Income part
+document.getElementById("calculate-button-02").addEventListener("click", function(){
+    const incomeAmount = inputFiled("income-filed");
+    const savings = inputFiled("saving-input")
+    const savingAmount = expenses("saving-amount");
+    const updateBalance = expenses("remaining-balance");
+    const totalsaving = (savings / 100)*incomeAmount ;
+    savingAmount.innerText = totalsaving;
+    const totalExpenses = totalExpensesAmount();
+    const remainingBalance = incomeAmount - totalExpenses;
+    const totalRemainingBalance = remainingBalance - totalsaving;
+    updateBalance.innerText = totalRemainingBalance;
     
 })
